b4a50e316798e95358208fbdd5db58f584b22f91
sougabriel <gabriel.dsouzapro@gmail.com>
2023-10-26 15:31:09 -0300

    Adicionado componentes inicio, sobre e entrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { EntrarComponent } from './forms/entrar/entrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    InicioComponent,
+    SobreComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/inicio/inicio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.less']
+})
+export class InicioComponent {
+
+}
