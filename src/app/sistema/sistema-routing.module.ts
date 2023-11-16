cf112fbd16213739d1569b28c9dc41b2ad45a58b
sougabriel <gabriel.dsouzapro@gmail.com>
2023-11-16 14:33:49 -0300

    Atualizado alguns estilos e dashboard layout

--- src/app/sistema/sistema-routing.module.ts
+++ src/app/sistema/sistema-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/sistema', pathMatch: 'full' },
+  { path: 'sistema', title: 'Sistema', component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
