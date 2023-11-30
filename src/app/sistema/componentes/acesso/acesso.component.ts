30a0c00d1fcebd69afcca531aa461bcca52df13b
sougabriel <gabriel.dsouzapro@gmail.com>
2023-11-30 18:06:22 -0300

    Adicionado insert de acesso

--- src/app/sistema/componentes/acesso/acesso.component.ts
+++ src/app/sistema/componentes/acesso/acesso.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Acesso } from '../../interfaces/acesso';
 import { AcessoService } from '../../services/routes/acesso.service';
 import { AtualizarService } from '../../services/atualizar.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-acesso',
@@ -10,11 +11,51 @@ import { AtualizarService } from '../../services/atualizar.service';
 })
 export class AcessoComponent {
 
+  acessoInForm!: FormGroup; 
+
   acessos: Acesso[] = [];
   colunasE: boolean = false;
 
   constructor (private acessoService: AcessoService, private atualizar: AtualizarService) {
     this.getAcessos();
+  }
+
+  ngOnInit(): void {
+    this.acessoInForm = new FormGroup({
+      id: new FormControl(),
+      dia: new FormControl(),
+      horaEntrada: new FormControl(),
+      finalidade: new FormControl(),
+      idPessoa: new FormControl(),
+    });
+  }
+
+  get id() {
+    return this.acessoInForm.get('id');
+  }
+
+  get dia() {
+    return this.acessoInForm.get('dia')!;
+  }
+
+  get horaEntrada() {
+    return this.acessoInForm.get('horaEntrada')!;
+  }
+
+  get finalidade() {
+    return this.acessoInForm.get('finalidade')!;
+  }
+
+  get idPessoa() {
+    return this.acessoInForm.get('idPessoa')!;
+  }
+
+  submit() {
+    if (this.acessoInForm.invalid) {
+      return;
+    }
+    this.acessoService.adicionar(this.acessoInForm.value).subscribe();
+    this.recaregarTabela();
   }
 
   editarAcesso(acesso: Acesso) {
