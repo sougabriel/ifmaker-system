b56051c01eeeef3e9083342bd1a46332bc3d62e8
sougabriel <gabriel.dsouzapro@gmail.com>
2023-12-07 00:50:06 -0300

    Adicionado método de logoff do sistema

--- src/app/sistema/dashboard/dashboard.component.ts
+++ src/app/sistema/dashboard/dashboard.component.ts
@@ -6,22 +6,37 @@ import { MensagensService } from 'src/app/services/mensagens.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.less']
+  styleUrls: ['./dashboard.component.less'],
 })
 export class DashboardComponent {
-
   list: string[];
   menu: boolean = true;
 
-  constructor(private router: Router, private localStorage: LocalStorageService, private mensagem: MensagensService) {
-    this.list = ["acesso", "emprestimo", "material", "pessoa", "projeto", "usuario"];
+  constructor(
+    private router: Router,
+    private localStorage: LocalStorageService,
+    private mensagem: MensagensService
+  ) {
+    this.list = [
+      'acesso',
+      'emprestimo',
+      'material',
+      'pessoa',
+      'projeto',
+      'usuario',
+    ];
   }
 
   ngOnInit() {
     if (this.localStorage.get('usuario') == null) {
-      this.mensagem.adicionar('Necessario efetuar login para acessar! ')
+      this.mensagem.adicionar('Necessario efetuar login para acessar! ');
       this.router.navigate(['/entrar']);
     }
   }
 
+  sair() {
+    this.localStorage.clear();
+    this.mensagem.adicionar('Você saiu do sistema! ');
+    this.router.navigate(['/inicio']);
+  }
 }
